1f2c1efd76ed588fde2873782a82a6229f76a78c
Travis Clinkscales <clink73@gmail.com>
2021-03-06 09:49:14 -0600

    Fixed function return in netlify function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,22 @@
-exports.handler = (event, context) => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    message: 'Hi there Tacos',
-    event,
-  }),
-});
+exports.handler = (event, context) => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hi there Tacos',
+        event,
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      headers: {
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify({
+        error: 'Unable to to get tacos...',
+      }),
+    };
+  }
+};
